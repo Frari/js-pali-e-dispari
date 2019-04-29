2651ba1fd89cf1f866a074ad277496175bebea59
Frari <francescoriccardi86@gmail.com>
2019-04-29 16:31:00 +0200

    inversione parola

--- pali/main.js
+++ pali/main.js
@@ -1,7 +1,13 @@
 // chiedo all'utente la parola da inserire
 var utente=prompt('inserisci parola');
 // controllo che la parola sia palidroma
-for(var i=utente.length;i>0;i--){
-  console.log(utente);
+
+for(var i=utente.length-1;i>=0;i--){
+  console.log(utente.charAt(i));
+  document.writeln(utente.charAt(i));
+}if(utente==utente.chatAt(i)){
+  console.log('la parola è palindroma');
+}else{
+  console.log('la parola non è palindroma');
 }
 // informo l'utente sul risultato
