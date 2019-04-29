c92795ab1605354e62b92bfda9d182e0b46c6680
Frari <francescoriccardi86@gmail.com>
2019-04-29 17:32:58 +0200

    fine

--- pali/main.js
+++ pali/main.js
@@ -1,12 +1,11 @@
 // chiedo all'utente la parola da inserire
 var utente=prompt('inserisci parola');
 
-var contrario=utente.charAt(i);
+var contrario='';
 
 // controllo che la parola sia palidroma
 for(var i=utente.length-1;i>=0;i--){
-  console.log(utente.charAt(i));
-  document.writeln(utente.charAt(i));
+  contrario+=utente.charAt(i);
 }
 
 // informo l'utente sul risultato
--- pariDispari/main.js
+++ pariDispari/main.js
@@ -1,17 +1,18 @@
 // chiedere a utente se pari o dispari
-var utente_pd=prompt('scegli pari o dispari?');
-var pari=(%2=0);
-var dispari=(%2!=0);
+var utente_pd=parseInt(prompt('scegli 2:pari o 1:dispari?'));
+
 
 // chiedere all'utente numero
-var utente_num=parsInt(prompt('scegli un numero da 1 a 5'));
+var utente_num=parseInt(prompt('scegli un numero da 1 a 5'));
 
 // generare numero computer random
 var computer=Math.floor(Math.random()*5)+1;
+ alert(computer);
+var somma=utente_num+computer;
 // confrontare i due lanci e dire chi ha vinto
-if(utente_pd==pari && utente_num+computer=pari){
+if(utente_pd==2 && somma %2==0){
   console.log('hai vinto');
-}else if(utente_pd==dispari && utente_num+computer=dispari){
+}else if(utente_pd==1 && somma %2==1){
   console.log('hai vinto');
 }else{
   console.log('hai perso');
