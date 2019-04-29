7cfc99a5052d8cba958f0db91ad8c00522e75066
Frari <francescoriccardi86@gmail.com>
2019-04-29 16:41:00 +0200

    risultato sempre negativo

--- pali/main.js
+++ pali/main.js
@@ -1,13 +1,17 @@
 // chiedo all'utente la parola da inserire
 var utente=prompt('inserisci parola');
-// controllo che la parola sia palidroma
 
+var contrario=utente.charAt(i);
+
+// controllo che la parola sia palidroma
 for(var i=utente.length-1;i>=0;i--){
   console.log(utente.charAt(i));
   document.writeln(utente.charAt(i));
-}if(utente==utente.chatAt(i)){
+}
+
+// informo l'utente sul risultato
+if(utente==contrario){
   console.log('la parola è palindroma');
 }else{
   console.log('la parola non è palindroma');
 }
-// informo l'utente sul risultato
