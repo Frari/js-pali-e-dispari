e5be148fae4f9f447812c7b97188bb12772efed0
Frari <francescoriccardi86@gmail.com>
2019-04-29 16:16:08 +0200

    inizio

---
+++ pali/main.js
@@ -0,0 +1,7 @@
+// chiedo all'utente la parola da inserire
+var utente=prompt('inserisci parola');
+// controllo che la parola sia palidroma
+for(var i=utente.length;i>0;i--){
+  console.log(utente);
+}
+// informo l'utente sul risultato
